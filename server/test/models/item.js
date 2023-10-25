8fc42017186a1b84d1885854ffeba8a07df51b30
carerit <kevnli2002@gmail.com>
2023-10-24 20:52:00 -0700

    added test cases for database & api for fetching database items

--- server/test/models/item.js
+++ server/test/models/item.js
@@ -8,19 +8,40 @@ import models from '../../models/index.js';
 
 describe('models.Item', () => {
   beforeEach(async () => {
-    await helper.loadFixtures([]);
+    await helper.loadFixtures(['items']);
   });
 
   it('creates a new Item record', async () => {
-    assert.deepStrictEqual(await models.Item.count(), 0);
+    assert.deepStrictEqual(await models.Item.count(), 4);
     const record = await models.Item.create({
       Title: 'This is a test title',
       Text: 'This is longer descriptive text',
     });
 
-    assert.deepStrictEqual(await models.Item.count(), 1);
-    assert.notDeepStrictEqual(record.id, null);
+    assert.deepStrictEqual(await models.Item.count(), 5);
     assert.deepStrictEqual(record.Title, 'This is a test title');
-    assert.deepStrictEqual(record.Text, 'This is a longer descriptive text');
+    assert.deepStrictEqual(record.Text, 'This is longer descriptive text');
+  });
+
+  it('finds an Item record by ID', async () => {
+    const record = await models.Item.findByPk(81273098);
+    assert.deepStrictEqual(record.Title, 'fixture title test 1');
+    assert.deepStrictEqual(record.Text, 'fixture text test 1');
+  });
+
+  it('finds multiple Item records', async () => {
+    const record = await models.Item.findAll({
+      order: [['Title', 'DESC']],
+    });
+
+    assert.deepStrictEqual(record.length, 4);
+    assert.deepStrictEqual(record[0].Title, 'fixture title test 4');
+  });
+
+  it('deletes an Item record', async () => {
+    assert.deepStrictEqual(await models.Item.count(), 4);
+    const record = await models.Item.findByPk(81273098);
+    await record.destroy();
+    assert.deepStrictEqual(await models.Item.count(), 3);
   });
 });
